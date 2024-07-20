65346719a731328792056e6169aa61a0c440d724
AndyDo0705 <106494340+AndyDo0705@users.noreply.github.com>
2024-07-20 21:36:07 +0700

    small changes

--- Assets/PipeParent.cs
+++ Assets/PipeParent.cs
@@ -6,12 +6,6 @@ public class PipeParent : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float deadZone = -45;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
